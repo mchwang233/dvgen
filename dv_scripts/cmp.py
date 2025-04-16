817666a279d4c38201550809158314aad73360b3
Gavin <mchwang@foxmail.com>
2025-04-16 07:15:04 +0000

    up

--- dv_scripts/cmp.py
+++ dv_scripts/cmp.py
@@ -4,6 +4,7 @@
 def get_cmp_cmd() :
     rtl_files = '${RTL_HOME}/dut.f'
     tb_files  = '../cfg/tb.f'
+    
     tb_top    = 'harness'
     
     cmp_cmd = 'vcs -ntb_opts uvm-1.2 -full64 -sverilog -debug_access+all -kdb -lca -timescale=1ps/1fs'
@@ -11,4 +12,5 @@ def get_cmp_cmd() :
     cmp_cmd = cmp_cmd + ' -F '+rtl_files 
     cmp_cmd = cmp_cmd + ' -f '+tb_files
     cmp_cmd = cmp_cmd + ' -top '+ tb_top
+
     return cmp_cmd
