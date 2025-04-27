bf8babf69174382abb8b6de2b84a96a760944294
Gavin <mchwang@foxmail.com>
2025-04-27 08:23:35 +0000

    up

--- bin/dvgen.py
+++ bin/dvgen.py
@@ -3,15 +3,21 @@
 import argparse
 import jinja2
 
+def do_ut(name):
+    print("UT generation start")
+
+
 def main():
     parser = argparse.ArgumentParser(description="DVGen script")
-    parser.add_argument('-uvm', action='store_true', help='Enable UVM generation')
+    parser.add_argument('-ut', action='store_true', help='Enable UVM generation')
+    parser.add_argument('-it', action='store_true', help='Enable UVM generation')
+    parser.add_argument('-st', action='store_true', help='Enable UVM generation')
+    
     parser.add_argument('-name', type=str, required=True, help='Specify the name')
     args = parser.parse_args()
 
-    if args.uvm:
-        print("UVM generation enabled")
-        print(args.name)
+    if args.ut: 
+        do_ut(args.name)
 
 if __name__ == "__main__":
     main()
